c71730a0b5e1678544b1c9aea8d471bdf5f13678
+Michael Kolb <+michael.w.kolb@gmail.com>
2021-11-02 15:15:11 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat() {
+    cats.push(`Ralph`);
+}
+
+function destructivelyPrependCat() {
+    cats.unshift(`Bob`)
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name =`Broom`) {
+    const copyOfCats = [...cats, name];
+    return copyOfCats;
+}
+
+function prependCat(name="Arnold") {
+    const copyOfCats = [name, ...cats];
+    return copyOfCats;
+}
+
+function removeLastCat() {
+    const copyOfCats = cats.slice(0, 2);
+    return copyOfCats;
+}
+
+function removeFirstCat() {
+    const copyOfCats = cats.slice(1);
+    return copyOfCats;
+}
